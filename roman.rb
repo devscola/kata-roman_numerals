f5554c37bb9f48064e3b30f59e6d769b642b1fc9
XaV1Uzz <xaviuzz@gmail.com>
2014-07-07 17:09:00 +0200

    A citizen of Rome When asked to translate an arabic numeral replies 'I' to 1 RED

--- roman.rb
+++ roman.rb
@@ -1,4 +1,6 @@
 class Roman
 
+  def self.translate arabic
+  end
   
 end
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -1,5 +1,9 @@
 require './roman'
 
 describe "A citizen of Rome" do
-
+	context 'When asked to translate an arabic numeral' do
+		it "replies 'I' to 1" do
+			expect(Roman.translate(1)).to eq "I"
+		end
+	end	
 end
