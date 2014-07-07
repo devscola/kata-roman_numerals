b4c2a06ddba31e571200d2203971d1aa5d51bc7f
XaV1Uzz <xaviuzz@gmail.com>
2014-07-07 17:12:47 +0200

    A citizen of Rome When asked to translate an arabic numeral replies 'II' to 2 RED

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -5,5 +5,8 @@ describe "A citizen of Rome" do
 		it "replies 'I' to 1" do
 			expect(Roman.translate(1)).to eq "I"
 		end
+    it "replies 'II' to 2" do
+      expect(Roman.translate(2)).to eq "II"
+    end
 	end	
 end
