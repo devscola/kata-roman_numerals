728745d5cb61da32f99560e3f8b69fdb11ce691f
XaV1Uzz <xaviuzz@gmail.com>
2014-07-07 17:51:36 +0200

    A citizen of Rome When asked to translate an arabic numeral replies properly to six REFAC

--- roman.rb
+++ roman.rb
@@ -3,11 +3,19 @@ class Roman
   FIVE="V"
   SPECIAL_CASE=4
 
-  def self.translate arabic
+  def translate arabic
     return UNIT+FIVE if arabic == SPECIAL_CASE
-    return FIVE if arabic == 5
-    return FIVE+UNIT if arabic == 6 
-    UNIT*arabic
+    
+    @resto = arabic
+    result = "" + reply_five_if_needed
+    result += UNIT * @resto
   end
   
+  def reply_five_if_needed
+    return "" if @resto < 5
+    @resto = @resto - 5
+    FIVE
+  end
+
 end
+
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -2,22 +2,24 @@ require './roman'
 
 describe "A citizen of Rome" do
 	context 'When asked to translate an arabic numeral' do
+    roman = Roman.new
+
 		it "replies properly till three" do
-			expect(Roman.translate(1)).to eq "I"
-      expect(Roman.translate(2)).to eq "II"
-      expect(Roman.translate(3)).to eq "III"
+			expect(roman.translate(1)).to eq "I"
+      expect(roman.translate(2)).to eq "II"
+      expect(roman.translate(3)).to eq "III"
 		end
 
     it "replies properly to four" do
-      expect(Roman.translate(4)).to eq "IV"
+      expect(roman.translate(4)).to eq "IV"
     end
 
     it "replies properly to five" do
-      expect(Roman.translate(5)).to eq "V"
+      expect(roman.translate(5)).to eq "V"
     end
 
     it "replies properly to six" do
-      expect(Roman.translate(6)).to eq "VI"
+      expect(roman.translate(6)).to eq "VI"
     end
 	end	
 end
